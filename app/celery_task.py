e38c061d1689db7ec2e8116ba37dbab52bd4e2a1
yzz <13135673615@163.com>
2021-07-12 10:14:49 +0800

    # 应用上下文

--- app/celery_task.py
+++ app/celery_task.py
@@ -2,7 +2,6 @@ import celery
 from celery import Celery
 
 
-
 class BaseTask(celery.Task):
 
     def on_failure(self, exc, task_id, args, kwargs, einfo):
@@ -10,7 +9,14 @@ class BaseTask(celery.Task):
 
     # 任务成功时执行
     def on_success(self, retval, task_id, args, kwargs):
+        from app import flask_app
+        from app.models import Article,db
         print('***')
+        with flask_app.app_context():
+            art = Article(title='test', content='zzzzz')
+            db.session.add(art)
+            db.session.commit()
+            print(Article.query.all())
         print(retval)
         print(task_id)
         print(args)
